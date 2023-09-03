fb241d1a892f04e8698cd7de4b2a1c698cc05b95
jraijman <jraijman@frba.utn.edu.ar>
2023-09-03 18:03:24 +0000

    nueva estructura

---
+++ utils/settings.mk
@@ -0,0 +1,13 @@
+# Libraries
+LIBS=commons
+
+# Custom libraries' paths
+SHARED_LIBPATHS=
+STATIC_LIBPATHS=
+
+# Compiler flags
+CDEBUG=-g -Wall -DDEBUG -fcommon -fdiagnostics-color=always
+CRELEASE=-O3 -Wall -DNDEBUG -fcommon
+
+# Source files (*.c) to be excluded from tests compilation
+TEST_EXCLUDE=
