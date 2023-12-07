131b67601ffe4626f3df62fbaae34211784f5338
jraijman <jraijman@frba.utn.edu.ar>
2023-12-07 14:30:32 +0000

    prueba deploy

--- kernel/settings.mk
+++ kernel/settings.mk
@@ -10,7 +10,7 @@ CDEBUG=-g -Wall -DDEBUG -fcommon -fdiagnostics-color=always
 CRELEASE=-O3 -Wall -DNDEBUG -fcommon
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=kernel.config
+ARGS=./config/kernel.config
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes
--- memoria/settings.mk
+++ memoria/settings.mk
@@ -10,7 +10,7 @@ CDEBUG=-g -Wall -DDEBUG -fcommon -fdiagnostics-color=always
 CRELEASE=-O3 -Wall -DNDEBUG -fcommon
 
 # Arguments when executing with start, memcheck or helgrind
-ARGS=memoria.config
+ARGS=./config/memoria.config
 
 # Valgrind flags
 MEMCHECK_FLAGS=--track-origins=yes
